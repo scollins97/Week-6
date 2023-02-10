5a547615458a94282a0063ccf3e03d83dfe90611
Sean Collins <SCollins@omni.local>
2023-02-10 13:10:05 -0500

    work in progress

--- WAR.js
+++ WAR.js
@@ -6,20 +6,64 @@ class Card{
     getValue(){
         return this.value;
     }
+    //this is mainly to make testing a little easier to read but could 
+    //be used as a play by play
+    describe(){
+        switch(this.value){
+            case 2:
+                console.log(`Two of ${suit}`);
+                break;
+            case 3:
+                console.log(`Three of ${suit}`);
+                break;
+             case 4:
+                console.log(`Four of ${suit}`);
+                break;
+            case 5:
+                console.log(`Five of ${suit}`);
+                break;
+            case 6:
+                console.log(`Six of ${suit}`);
+                break;
+            case 7:
+                console.log(`Seven of ${suit}`);
+                break;
+            case 8:
+                console.log(`Eight of ${suit}`);
+                break;
+            case 9:
+                console.log(`Nine of ${suit}`);
+                break;
+            case 10:
+                console.log(`Ten of ${suit}`);
+                break;
+            case 11:
+                console.log(`Jack of ${suit}`);
+                break;
+            case 12:
+                console.log(`Queen of ${suit}`);
+                break;
+            case 13:
+                console.log(`King of ${suit}`);
+                break;
+            case 14:
+                console.log(`Ace of ${suit}`);
+                break;
+
+        }
+    }
 }
 class Deck{
     constructor(cards){
-        this.cards = [filldeck()];
+        this.cards = [];
     }
     fillDeck(){
-        let newDeck = [];
         let cardSuits = ["Hearts" , "Spades" , "Diamonds" , "Clubs"];
         for(let i = 0; i < 4; i ++) {
             for(let j = 2; j <= 14; j++) {
-                this.newDeck.push(new Card(j, cardSuits[i]));
+                this.cards.push(new Card(j, cardSuits[i]));
             }
         }
-        return newDeck;
     }
     shuffle() {
         shuffle(this.cards);
@@ -57,6 +101,5 @@ class Player{
 
 //this is just testing that my deck fills and can be printed
 let deck = new Deck();
-deck.cards.forEach(element => {
-    console.log(element);
-});
+deck.fillDeck();
+deck.cards.forEach(element => (element.describe));
